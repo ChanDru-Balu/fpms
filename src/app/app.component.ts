7a8e0601380b4f8045088f8dc0a205906e42807c
ChanDru-Balu <prochandru@gmail.com>
2025-06-03 11:13:06 +0530

    interceptor with loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./features/layout/header/header.component";
+import { LoadingService } from './core/common/services/loading.service';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent , MatProgressSpinnerModule , CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  loadingService = inject(LoadingService)
+
   title = 'fpms';
 }
