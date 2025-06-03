645a5d5326ca6c0b9db512a2551ef375c06b34be
ChanDru-Balu <prochandru@gmail.com>
2025-06-03 11:03:04 +0530

    review form

--- src/app/features/investment/add/add.component.ts
+++ src/app/features/investment/add/add.component.ts
@@ -1,16 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { assetClass } from '../../../core/common/interface/asset.interface';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { TextCaseDirective } from '../../../core/common/directive/text-case.directive';
-import {MatSnackBarModule , MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
+import { ReviewComponent } from '../review/review.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add',
-  imports: [CommonModule , ReactiveFormsModule , MatFormFieldModule , MatInputModule, MatSelectModule , TextCaseDirective , MatSnackBarModule ],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, TextCaseDirective, MatSnackBarModule],
   templateUrl: './add.component.html',
   styleUrl: './add.component.scss'
 })
@@ -18,26 +20,40 @@ export class AddComponent implements OnInit {
 
   fb = inject(FormBuilder);
   snackBar = inject(MatSnackBar);
+  dialog = inject(MatDialog);
 
   assetClass = assetClass;
   assetClassOptions = Object.values(assetClass);
 
   investForm = this.fb.group({
-    assetType : ['',[Validators.required]],
-    quantity : ['',[Validators.required]],
-    purchasePrice: ['',[Validators.required]],
-    date: [new Date(),[Validators.required]] 
+    assetType: ['', [Validators.required]],
+    quantity: ['', [Validators.required]],
+    purchasePrice: ['', [Validators.required]],
+    date: [new Date(), [Validators.required]]
   })
 
-  ngOnInit(){
+  ngOnInit() {
     this.assetClassOptions;
   }
 
   addInvestment() {
-    if(!this.investForm.valid){
-      this.snackBar.open('Please fill all fields', 'X', {duration: 3000});
+    if (!this.investForm.valid) {
+      this.snackBar.open('Please fill all fields', 'X', { duration: 3000 });
     }
-    console.log('Form:',this.investForm.value)
+    console.log('Form:', this.investForm.value)
+    const dialogRef = this.dialog.open(ReviewComponent, {
+      width: '400px',
+      data: this.investForm.value
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // This will run after the dialog is closed
+      console.log('Dialog closed with result:', result);
+      this.snackBar.open('Investment added into portfolio','X',{duration: 2000})
+      this.investForm.reset();
+    });
+
+
   }
 
 
--- src/app/features/investment/review/review.component.ts
+++ src/app/features/investment/review/review.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-review',
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './review.component.html',
   styleUrl: './review.component.scss'
 })
 export class ReviewComponent {
+
+  longText: any = 'Test';
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private dialogRef: MatDialogRef<ReviewComponent>) {}
+  objectKeys = Object.keys;
+
+  reviewed() {
+    this.dialogRef.close('confirm');
+  }
 
 }
